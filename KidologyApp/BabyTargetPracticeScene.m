4354910c96305a3bd51058a4b330dbf7846344f2
ngo <ngot@mac-cl1160.cse.ohio-state.edu>
2014-02-27 20:07:31 -0500

    temp commit

--- KidologyApp/BabyTargetPracticeScene.m
+++ KidologyApp/BabyTargetPracticeScene.m
@@ -10,14 +10,14 @@
 
 @implementation BabyTargetPracticeScene
 
--(id)initWithSize:(CGSize)size
+-(id)initWithSize:(CGSize)size //color:(NSString *)color
 {
 if (self = [super initWithSize:size])
     {
         SKSpriteNode *bgImage = [SKSpriteNode spriteNodeWithImageNamed:@"Huge_Checkered_Background_[4096x3072]"];
         [self addChild:bgImage];
         
-        _target = [SKSpriteNode spriteNodeWithImageNamed:@"green"];
+        _target = [SKSpriteNode spriteNodeWithImageNamed:@"Purple"];
         _target.xScale = .70;
         _target.yScale = .70;
         _target.position = CGPointMake(self.size.width/2, self.size.height/2);
@@ -31,16 +31,16 @@ if (self = [super initWithSize:size])
 {
     self.target.position = CGPointMake(self.size.width/2, self.size.height/2);
 }
-
--(void)hideTarget
-{
-    self.target.position = CGPointMake(self.size.width/2*(-1), self.size.height/2*(-1));
-}
+//
+//-(void)hideTarget
+//{
+//    self.target.position = CGPointMake(self.size.width/2*(-1), self.size.height/2*(-1));
+//}
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     CGPoint position = [touch locationInNode:self];
-    
+    [self targetTouch:position];
 }
 
 -(void)targetTouch:(CGPoint)touchLocation
@@ -48,13 +48,24 @@ if (self = [super initWithSize:size])
     //    NSLog(@"touch at (%f, %f).", touchLocation.x, touchLocation.y);
     double xDifference = touchLocation.x - self.target.position.x;
     double yDifference = touchLocation.y - self.target.position.y;
-    double radius = self.target.size.width / 2;
+    double radius = self.target.size.width / 2*.8387; //<--- The percentage of the radius that is the circle
     double leftHandSide = (pow(xDifference, 2) + pow(yDifference, 2));
     double rightHandSide = pow(radius, 2);
     
     if(leftHandSide <= rightHandSide) // If the touch is on the target
     {
-        
+        SKAction *deleteTarget = [SKAction runBlock:^{
+        self.target.position = CGPointMake(-100,-100);
+        }];
+        //make a wait action
+        SKAction *wait = [SKAction waitForDuration:1];
+        //make a "add" target action
+        SKAction *addTarget = [SKAction runBlock:^{
+            [self displayTarget];
+        }];
+
+        SKAction *showAnotherTarget = [SKAction sequence:@[deleteTarget,wait,addTarget]];
+        [self runAction:[SKAction repeatAction:showAnotherTarget count:1]];
     }
 }
 
