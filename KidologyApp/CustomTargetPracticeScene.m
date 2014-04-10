505aa4c2530f4492034145c96ef108a6bba888a7
klimczak <klimczak@mac-cl1158.cse.ohio-state.edu>
2014-04-10 10:44:12 -0400

    fixed some cdv reading stuff

--- KidologyApp/CustomTargetPracticeScene.m
+++ KidologyApp/CustomTargetPracticeScene.m
@@ -48,17 +48,14 @@ NSMutableArray *touchLog;
     NSString *targetInfoString = _commandArray[_targetIterator];
     //array of size 3 where [0] = X position [1] = Y position [2] = scale of target
     NSArray *targetInfoArray = [targetInfoString componentsSeparatedByString:@","];
-    NSLog(@"%@", targetInfoArray);
     //set position based and scale on the values
     _target.xScale = [targetInfoArray[2] floatValue];
     _target.yScale = [targetInfoArray[2] floatValue];
     _target.position = CGPointMake([targetInfoArray[0] floatValue], [targetInfoArray[1] floatValue]);
-    NSLog(@"%@", targetInfoArray);
-    if([targetInfoArray count] == 4)
+    if(!([targetInfoArray[3] isEqualToString:@""]))
     {
         _delayDuration = [targetInfoArray[3] floatValue];
     }
-    NSLog(@"%f", _delayDuration);
 }
 
 -(void)readInput
@@ -72,6 +69,7 @@ NSMutableArray *touchLog;
         NSLog(@"erorr %@", error.localizedDescription);
     }
     _commandArray = [fileContents componentsSeparatedByString:@"\n"];
+    NSLog(@"%@", _commandArray);
 
 
 }
